8dbefd725a7695fd3f3c1963158a3d5ad06e04f0
Will Greenberg <greenberg.will@gmail.com>
2022-04-19 09:30:48 -0700

    add components, add conditional rendering, edit handle click to cycle through numbers instead of bool

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,31 +1,48 @@
 import React from 'react';
 import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
+import Debug from './Debug';
+import Pair from './Pair';
+import Fifteen from './Fifteen';
 
 class TicketControl extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      visibleOnPage: 0
     };
   }
 
   handleClick = () => {
     this.setState(prevState => ({
-      formVisibleOnPage: !prevState.formVisibleOnPage
+      visibleOnPage: prevState.visibleOnPage + 1
     }));
+    if (this.state.visibleOnPage > 3){
+      this.setState(() => ({
+        visibleOnPage: 0
+      }))
+    }
   }
 
   render(){
     let currentlyVisibleState = null;
     let buttonText = null;
-    if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTicketForm />
-      buttonText = "Return to Ticket List";
-    } else {
+    if (this.state.visibleOnPage === 0) {
       currentlyVisibleState = <TicketList />
       buttonText = "Add Ticket";
+    } else if(this.state.visibleOnPage === 1){
+      currentlyVisibleState = <Debug />
+      buttonText = "Yes";
+    } else if (this.state.visibleOnPage === 2){
+      currentlyVisibleState = <Pair />
+      buttonText = "Yes";
+    } else if (this.state.visibleOnPage === 3){
+      currentlyVisibleState = <Fifteen />
+      buttonText = "Yes";
+    } else {
+      currentlyVisibleState = <NewTicketForm />
+      buttonText = "Return to Ticket List";
     }
     return (
       <React.Fragment>
